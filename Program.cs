d072e38d10b49adbf7d7a4c1be7849874fd1dd2f
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-22 18:41:42 +0200

    changes made added ecamople to read from file and to write in file

--- Program.cs
+++ Program.cs
@@ -12,15 +12,50 @@ namespace StreamReadWriter
     {
         static void Main()
         {
-            var writer = new StreamWriter("numbers.txt");  //Writes a loop result in a file into the computer
+            using System;
+            using System.IO;
+            using System.Threading.Tasks;
+            using System.Collections.Generic;
+            using System.Linq;
+            using System.Text;
 
-            using (writer)
+
+namespace StreamReadWriter
+    {
+        class Program
+        {
+            static void Main()
             {
-                for (int i = 0; i < 1000; i++)
+
+
+                var reader = new StreamReader(@"..\File1.txt");   ///Reading from File 
+                using (reader)
                 {
-                    writer.Write(i + " ");
+                    string currentLine = reader.ReadLine();
+                    int count = 1;
+                    while (currentLine != null)
+                    {
+                        Console.WriteLine("{0}. {1}", count, currentLine);
+                        count++;
+                        currentLine = reader.ReadLine();
+                    }
+                }
+
+
+                var writer = new StreamWriter("numbers.txt");  //Writes a loop result in a file into the computer
+
+                using (writer)  /Writing result in a File //
+                {
+                    for (int i = 0; i < 1000; i++)
+                    {
+                        writer.Write(i + " ");
+                    }
                 }
             }
+        }
+    }
+
+}
         }
     }
 }
