d9ab8dd7e804b9002c7f0399b476dde314d5b86e
Blagovest-Todorov <blagovest.stod@abv.bg>
2020-11-22 18:25:15 +0200

    init

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace StreamReadWriter
+{
+    class Program
+    {
+        static void Main()
+        {
+            var writer = new StreamWriter("numbers.txt");  //Writes a loop result in a file into the computer
+
+            using (writer)
+            {
+                for (int i = 0; i < 1000; i++)
+                {
+                    writer.Write(i + " ");
+                }
+            }
+        }
+    }
+}
